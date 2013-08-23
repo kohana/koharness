7a556d09fe6f0d6b2c88ed1b0ad434662422657d
Andrew Coulton <andrew@ingenerator.com>
2013-08-23 14:17:20 +0100

    Add a Session_Fake driver to mock session storage for use in tests
    Note that this class is not autoloaded - if you need it, include it
    manually into your spec file etc - or register an autoloader if you
    want one.

---
+++ helper_classes/Session/Fake.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Fake session driver that just stores the session data in memory in the instance - so there are no issues with
+ * setting cookies etc during tests.
+ *
+ * @author    Andrew Coulton <andrew@ingenerator.com>
+ * @copyright 2013 inGenerator Ltd
+ * @licence   BSD
+ */
+
+/**
+ * Fake session driver that stores values in the instance.
+ */
+class Session_Fake extends Session {
+
+	/**
+	 * @var string the serialised data
+	 */
+	protected $data = NULL;
+
+	/**
+	 * @var string the session ID
+	 */
+	protected $id = NULL;
+
+	/**
+	 * Loads the raw session data string and returns it.
+	 *
+	 * @param   string $id session id
+	 *
+	 * @return  string
+	 */
+	protected function _read($id = NULL)
+	{
+		$this->id = $id;
+		return $this->data;
+	}
+
+	/**
+	 * Generate a new session id and return it.
+	 *
+	 * @return  string
+	 */
+	protected function _regenerate()
+	{
+		$this->id++;
+	}
+
+	/**
+	 * Writes the current session.
+	 *
+	 * @return  boolean
+	 */
+	protected function _write()
+	{
+		$this->data = $this->__toString();
+	}
+
+	/**
+	 * Destroys the current session.
+	 *
+	 * @return  boolean
+	 */
+	protected function _destroy()
+	{
+		$this->data = NULL;
+		return TRUE;
+	}
+
+	/**
+	 * Restarts the current session.
+	 *
+	 * @return  boolean
+	 */
+	protected function _restart()
+	{
+		return true;
+	}
+
+	/**
+	 * Retrieves the current session ID
+	 *
+	 * @return string
+	 */
+	public function id()
+	{
+		return $this->id;
+	}
+
+}
